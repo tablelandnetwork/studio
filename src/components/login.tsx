fe3941999b755998f283f4469cf4108ed3e8011a
Aaron Sutula <asutula@users.noreply.github.com>
2023-04-01 15:40:23 -0600

    install and wip on biconomy
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,10 +1,22 @@
 "use client";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
+import SocialLogin from "@biconomy/web3-auth";
+import "@biconomy/web3-auth/dist/src/style.css";
 import Button from "./button";
 
 export default function Login() {
+  const socialLogin = useRef(new SocialLogin());
+
+  useEffect(() => {
+    async function init() {
+      await socialLogin.current.init();
+    }
+    init();
+  }, []);
+
   const onClick = useCallback(() => {
     console.log("hi there");
+    socialLogin.current.showWallet();
   }, []);
   return <Button onClick={onClick}>Login</Button>;
 }
