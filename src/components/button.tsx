5f608481f12d38cf655b95d34f36b3218ae1599d
Aaron Sutula <asutula@users.noreply.github.com>
2023-04-01 15:39:57 -0600

    clean up button
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,43 +1,32 @@
 import { cva, VariantProps } from "class-variance-authority";
 
-const button = cva(["font-semibold", "border"], {
-  variants: {
-    intent: {
-      primary: [
-        "text-black",
-        "hover:bg-indigo-700",
-        "hover:text-white",
-        "border-black",
-        "hover:border-transparent",
-      ],
-      // **or**
-      // primary: "bg-blue-500 text-white border-transparent hover:bg-blue-600",
-      secondary: [
-        "bg-white",
-        "text-gray-800",
-        "border-gray-400",
-        "hover:bg-gray-100",
-      ],
+const button = cva(
+  [
+    "font-semibold",
+    "border",
+    "hover:bg-indigo-700",
+    "hover:border-transparent",
+    "uppercase",
+  ],
+  {
+    variants: {
+      intent: {
+        primary: ["bg-black", "text-white", "border-black", ,],
+        // **or**
+        // primary: "bg-blue-500 text-white border-transparent hover:bg-blue-600",
+        secondary: ["text-black", "hover:text-white", "border-black"],
+      },
+      size: {
+        small: ["text-sm", "py-1", "px-2"],
+        medium: ["text-base", "py-2", "px-4"],
+      },
     },
-    size: {
-      small: ["text-sm", "py-1", "px-2"],
-      medium: ["text-base", "py-2", "px-4"],
-    },
-  },
-  compoundVariants: [
-    {
+    defaultVariants: {
       intent: "primary",
       size: "medium",
-      class: "uppercase",
-      // **or** if you're a React.js user, `className` may feel more consistent:
-      // className: "uppercase"
     },
-  ],
-  defaultVariants: {
-    intent: "primary",
-    size: "medium",
-  },
-});
+  }
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
