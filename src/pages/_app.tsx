49c0e32122b71b07c0514671a8636f80333e6300
Aaron Sutula <asutula@users.noreply.github.com>
2023-04-18 21:44:52 -0600

    integrate trpc and replace old api routes
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
+import { trpc } from "@/utils/trpc";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
+
+export default trpc.withTRPC(App);
