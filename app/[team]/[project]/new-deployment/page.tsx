d29b653ab7851c345238d4bcd3ce1316db64455b
Allen Muncy <allen@textile.io>
2023-07-26 11:13:11 -0500

    Start on new deployment ui

---
+++ app/[team]/[project]/new-deployment/page.tsx
@@ -0,0 +1,43 @@
+import db from "@/db/api";
+import Session from "@/lib/session";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+
+import NewDeploymentForm from "@/components/new-deployment-form";
+
+export default async function NewProject({
+  params,
+}: {
+  params: { team: string; project: string };
+}) {
+  const session = await Session.fromCookies(cookies());
+  if (!session.auth) {
+    notFound();
+  }
+  const team = await db.teams.teamBySlug(params.team);
+  if (!team) {
+    notFound();
+  }
+  if (
+    !(await db.teams.isAuthorizedForTeam(session.auth.user.teamId, team.id))
+  ) {
+    notFound();
+  }
+  const project = await db.projects.projectByTeamIdAndSlug(
+    team.id,
+    params.project
+  );
+  if (!project) {
+    notFound();
+  }
+
+  const deployments = await db.deployments.deploymentsByProjectId(project.id);
+
+  const tables = await db.tables.tablesByProjectId(project.id);
+
+  return (
+    <div className="p-4">
+      <NewDeploymentForm team={team} project={project} tables={tables} />
+    </div>
+  );
+}
