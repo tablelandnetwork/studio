978d242db3d2ccedeee4eb7c3a1fee3149200425
Aaron Sutula <asutula@users.noreply.github.com>
2023-07-31 17:31:31 -0600

    revert unrelated change
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- app/[team]/(team)/people/page.tsx
+++ app/[team]/(team)/people/page.tsx
@@ -38,37 +38,11 @@ export default async function People({ params }: { params: { team: string } }) {
     notFound();
   }
 
-  // TODO: Update this query to return owner or not.
   const people = await db.teams.userTeamsForTeamId(team.id);
   const invites = await db.invites.invitesForTeam(team.id);
 
-  const claimedInvitesMap = invites.reduce((acc, invite) => {
-    if (invite.claimedBy) {
-      acc.set(invite.claimedBy.id, invite);
-    }
-    return acc;
-  }, new Map<string, (typeof invites)[number]>());
-
-  const peoplePlus = people.map((person) => {
-    if (claimedInvitesMap.has(person.personalTeam.id)) {
-      return {
-        ...person,
-        claimedInvite: claimedInvitesMap.get(person.personalTeam.id),
-      };
-    }
-    return {
-      ...person,
-      claimedInvite: undefined,
-    };
-  });
-
   return (
     <div className="grid items-start justify-center gap-6 self-center rounded-lg p-8 md:grid-cols-2">
-      {people.map((person) => {
-        return (
-          <div key={person.personalTeam.id}>{person.personalTeam.slug}</div>
-        );
-      })}
       <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
         <Container>
           <TeamMembers
