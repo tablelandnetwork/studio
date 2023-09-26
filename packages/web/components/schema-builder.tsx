4b6a25649f941567730d5400d49bf93c4e4dea8b
Aaron Sutula <asutula@users.noreply.github.com>
2023-09-26 11:27:37 -0600

    wip on new schema format
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- packages/web/components/schema-builder.tsx
+++ packages/web/components/schema-builder.tsx
@@ -7,8 +7,10 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { CreateTable, createTableAtom } from "@/store/create-table";
+import { Table as TblTable } from "@tableland/sdk";
 import { useAtom } from "jotai";
 import { Plus, X } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Input } from "./ui/input";
@@ -19,6 +21,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+
+type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
+
+type Schema = DeepWriteable<TblTable["schema"]>;
+
+const foo: Schema = {
+  columns: [
+    {
+      name: "",
+      type: "",
+    },
+  ],
+};
 
 function isValidColumnName(variable: string) {
   var columnNameRegex = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
@@ -73,11 +88,12 @@ export function createTableStatementFromObject(
 }
 
 export default function SchemaBuilder() {
-  const [tbl, setCreateTable] = useAtom(createTableAtom);
+  const [schema, setSchema] = useState<Schema>({ columns: [] });
+
   return (
     <div className="flex flex-col items-start">
       <Table>
-        {tbl.columns.length > 0 && (
+        {schema.columns.length > 0 && (
           <TableHeader>
             <TableRow>
               <TableHead>Name</TableHead>
@@ -91,7 +107,7 @@ export default function SchemaBuilder() {
           </TableHeader>
         )}
         <TableBody>
-          {tbl.columns.map((column, index) => {
+          {schema.columns.map((column, index) => {
             return <CreateColumn key={index} columnIndex={index} />;
           })}
         </TableBody>
@@ -102,18 +118,13 @@ export default function SchemaBuilder() {
         variant="outline"
         size="sm"
         onClick={() => {
-          setCreateTable((prev) => {
-            const newColumn = {
-              name: "",
-              type: "text",
-              notNull: false,
-              primaryKey: false,
-              unique: false,
-              default: null,
-            };
+          setSchema((prev) => {
             return {
               ...prev,
-              columns: [...prev.columns, newColumn],
+              columns: [
+                ...prev.columns,
+                { name: "", type: "text", constraints: [] },
+              ],
             };
           });
         }}
@@ -123,140 +134,151 @@ export default function SchemaBuilder() {
       </Button>
     </div>
   );
+
+  function RemoveColumn({ columnIndex }: { columnIndex: number }) {
+    const [tbl, setAtom] = useAtom(createTableAtom);
+    return (
+      <Button
+        type="button"
+        variant="ghost"
+        size="sm"
+        onClick={() => {
+          setAtom((prev) => {
+            prev.columns.splice(columnIndex, 1);
+            return {
+              ...prev,
+            };
+          });
+        }}
+      >
+        <X />
+      </Button>
+    );
+  }
+
+  function CreateColumn({ columnIndex }: { columnIndex: number }) {
+    const column = schema.columns[columnIndex];
+
+    return (
+      <TableRow>
+        <TableCell>
+          <Input
+            className="w-28"
+            placeholder="column_name"
+            pattern="[a-zA-Z0-9_]*"
+            name="name"
+            title={
+              "Letter, numbers, and underscores only. First character cannot be a number"
+            }
+            value={column.name}
+            onChange={(e) => {
+              setSchema((prev) => {
+                prev.columns[columnIndex].name = e.target.value;
+                return {
+                  ...prev,
+                };
+              });
+            }}
+          />
+        </TableCell>
+        <TableCell>
+          <Select
+            defaultValue={column.type}
+            onValueChange={(e) => {
+              setSchema((prev) => {
+                prev.columns[columnIndex].type = e;
+                return {
+                  ...prev,
+                };
+              });
+            }}
+          >
+            <SelectTrigger className="gap-x-2">
+              <SelectValue placeholder="Select a type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="text">Text</SelectItem>
+              <SelectItem value="integer">Integer</SelectItem>
+              <SelectItem value="int">Int</SelectItem>
+              <SelectItem value="blob">Blob</SelectItem>
+            </SelectContent>
+          </Select>
+        </TableCell>
+        <TableCell>
+          <Checkbox
+            name="notNull"
+            checked={hasNotNull(column)}
+            onCheckedChange={(state) => {
+              setSchema((prev) => {
+                setNotNull(prev.columns[columnIndex], !!state);
+                return {
+                  ...prev,
+                };
+              });
+            }}
+          />
+        </TableCell>
+        <TableCell>
+          <Checkbox
+            name="primaryKey"
+            checked={column.primaryKey}
+            onCheckedChange={(state) => {
+              setCreateTable((prev) => {
+                prev.columns[columnIndex].primaryKey = !!state;
+                return {
+                  ...prev,
+                };
+              });
+            }}
+          />
+        </TableCell>
+        <TableCell>
+          <Checkbox
+            name="unique"
+            checked={column.unique}
+            onCheckedChange={(state) => {
+              setCreateTable((prev) => {
+                prev.columns[columnIndex].unique = !!state;
+                return {
+                  ...prev,
+                };
+              });
+            }}
+          />
+        </TableCell>
+        <TableCell>
+          <Input
+            className="w-20"
+            type="text"
+            name="default"
+            placeholder="null"
+            defaultValue={column.default}
+            onChange={(e) => {
+              setCreateTable((prev) => {
+                prev.columns[columnIndex].default = e.target.value;
+                return {
+                  ...prev,
+                };
+              });
+            }}
+          />
+        </TableCell>
+        <TableCell>
+          <RemoveColumn columnIndex={columnIndex} />
+        </TableCell>
+      </TableRow>
+    );
+  }
 }
 
-function RemoveColumn({ columnIndex }: { columnIndex: number }) {
-  const [tbl, setAtom] = useAtom(createTableAtom);
-  return (
-    <Button
-      type="button"
-      variant="ghost"
-      size="sm"
-      onClick={() => {
-        setAtom((prev) => {
-          prev.columns.splice(columnIndex, 1);
-          return {
-            ...prev,
-          };
-        });
-      }}
-    >
-      <X />
-    </Button>
-  );
+function hasNotNull(column: Schema["columns"][number]) {
+  return column.constraints?.includes("NOT NULL");
 }
 
-function CreateColumn({ columnIndex }: { columnIndex: number }) {
-  const [tbl, setCreateTable] = useAtom(createTableAtom);
-  const column = tbl && tbl.columns[columnIndex];
-
-  return (
-    <TableRow>
-      <TableCell>
-        <Input
-          className="w-28"
-          placeholder="column_name"
-          pattern="[a-zA-Z0-9_]*"
-          name="name"
-          title={
-            "Letter, numbers, and underscores only. First character cannot be a number"
-          }
-          value={column.name}
-          onChange={(e) => {
-            setCreateTable((prev) => {
-              prev.columns[columnIndex].name = e.target.value;
-              return {
-                ...prev,
-              };
-            });
-          }}
-        />
-      </TableCell>
-      <TableCell>
-        <Select
-          defaultValue={column.type}
-          onValueChange={(e) => {
-            setCreateTable((prev) => {
-              prev.columns[columnIndex].type = e;
-              return {
-                ...prev,
-              };
-            });
-          }}
-        >
-          <SelectTrigger className="gap-x-2">
-            <SelectValue placeholder="Select a type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="text">Text</SelectItem>
-            <SelectItem value="integer">Integer</SelectItem>
-            <SelectItem value="int">Int</SelectItem>
-            <SelectItem value="blob">Blob</SelectItem>
-          </SelectContent>
-        </Select>
-      </TableCell>
-      <TableCell>
-        <Checkbox
-          name="notNull"
-          checked={column.notNull}
-          onCheckedChange={(state) => {
-            setCreateTable((prev) => {
-              prev.columns[columnIndex].notNull = !!state;
-              return {
-                ...prev,
-              };
-            });
-          }}
-        />
-      </TableCell>
-      <TableCell>
-        <Checkbox
-          name="primaryKey"
-          checked={column.primaryKey}
-          onCheckedChange={(state) => {
-            setCreateTable((prev) => {
-              prev.columns[columnIndex].primaryKey = !!state;
-              return {
-                ...prev,
-              };
-            });
-          }}
-        />
-      </TableCell>
-      <TableCell>
-        <Checkbox
-          name="unique"
-          checked={column.unique}
-          onCheckedChange={(state) => {
-            setCreateTable((prev) => {
-              prev.columns[columnIndex].unique = !!state;
-              return {
-                ...prev,
-              };
-            });
-          }}
-        />
-      </TableCell>
-      <TableCell>
-        <Input
-          className="w-20"
-          type="text"
-          name="default"
-          placeholder="null"
-          defaultValue={column.default}
-          onChange={(e) => {
-            setCreateTable((prev) => {
-              prev.columns[columnIndex].default = e.target.value;
-              return {
-                ...prev,
-              };
-            });
-          }}
-        />
-      </TableCell>
-      <TableCell>
-        <RemoveColumn columnIndex={columnIndex} />
-      </TableCell>
-    </TableRow>
-  );
+function setNotNull(column: Schema["columns"][number], value: boolean) {
+  if (value) {
+    column.constraints?.push("NOT NULL");
+  } else {
+    column.constraints?.splice(column.constraints.indexOf("NOT NULL"), 1);
+  }
 }
