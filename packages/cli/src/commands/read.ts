2ec306e16a64de4b877b0b17b236d03c1acb4f7a
Joe Wagner <njwjs722@gmail.com>
2023-10-25 08:25:59 -0600

    Add tests for read command

--- packages/cli/src/commands/read.ts
+++ packages/cli/src/commands/read.ts
@@ -2,7 +2,6 @@ import type yargs from "yargs";
 import type { Arguments, CommandBuilder } from "yargs";
 import { Database } from "@tableland/sdk";
 import { studioAliases } from "@tableland/studio-client"
-import { z as zod } from "zod";
 import { type GlobalOptions } from "../cli.js";
 import {
   ERROR_INVALID_PROJECT_ID,
@@ -19,18 +18,14 @@ import {
 export const command = "read [query]";
 export const desc = "run a read query";
 
-const ensureString = zod.string().nonempty();
-
 export const handler = async (
   argv: Arguments<GlobalOptions>,
 ): Promise<void> => {
   try {
     const { store, query } = argv;
 
-    // TODO: I tried using zod for this, but getting type errors
-    //       when trying to `prepare` the query since query remains type unknown
-    //       must be doing something wrong.
-    if (typeof query !== "string") {
+    // TODO: open a PR where these checks are done with zod
+    if (typeof query !== "string" || query.trim() === "") {
       throw new Error("must provide query");
     }
 
@@ -40,19 +35,16 @@ export const handler = async (
     if (queryObject.type !== "read") {
       throw new Error("query must be a read query");
     }
+    if (typeof store !== "string" || store.trim() === "") {
+      throw new Error("must provide path to session store file");
+    }
 
     const fileStore = new FileStore(store as string);
     const apiUrl = getApiUrl({ apiUrl: argv.apiUrl, store: fileStore})
     const api = getApi(fileStore, apiUrl as string);
     const projectId = getProject({ ...argv, store: fileStore });
 
-    try {
-      ensureString.parse(projectId);
-    } catch (err) {
-      throw new Error(ERROR_INVALID_PROJECT_ID);
-    }
-
-    if (!isUUID(projectId)) {
+    if (typeof projectId !== "string" || !isUUID(projectId)) {
       throw new Error(ERROR_INVALID_PROJECT_ID);
     }
 
