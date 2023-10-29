efc64417f20bc3d1a05ebee3b90f9d6165e3de29
Joe Wagner <njwjs722@gmail.com>
2023-10-29 13:12:17 -0600

    (cli) rename read command to query

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,11 +1,11 @@
-import * as login from "./login.js";
-import * as logout from "./logout.js";
-import * as team from "./team.js";
-import * as project from "./project.js";
 import * as deployment from "./deployment.js";
 import * as importData from "./import-data.js";
-import * as read from "./read.js";
 import * as importTable from "./import-table.js";
+import * as login from "./login.js";
+import * as logout from "./logout.js";
+import * as project from "./project.js";
+import * as query from "./query.js";
+import * as team from "./team.js";
 import * as use from "./use.js";
 import * as unuse from "./unuse.js";
 
@@ -16,7 +16,7 @@ export const commands = [
   project,
   deployment,
   importData,
-  read,
+  query,
   importTable,
   use,
   unuse
