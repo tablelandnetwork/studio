c65848496d5c283d8edf5da033a5ff72661c3c04
Joe Wagner <njwjs722@gmail.com>
2023-12-22 10:09:07 -0700

    (web) fix error message overflow in deploy modal

--- packages/web/app/[team]/[project]/(project)/deployments/[[...slug]]/_components/exec-deployment.tsx
+++ packages/web/app/[team]/[project]/(project)/deployments/[[...slug]]/_components/exec-deployment.tsx
@@ -192,66 +192,73 @@ export default function ExecDeployment({
   return (
     <Dialog open={showDialog} onOpenChange={handleOnOpenChange}>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Deploy Table: {table.name}</DialogTitle>
+        <div className="flex flex-auto flex-col gap-y-4 overflow-auto">
+          <DialogHeader>
+            <DialogTitle>Deploy Table: {table.name}</DialogTitle>
+            <DialogDescription>
+              Deploying a Table to Tableland will require you to sign and send a
+              transaction, as well as pay any transaction fees. Once the Table
+              has been deployed, it will be registered with your Studio Project
+              and you&apos;ll be able to view it in the Deployments tab.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center gap-2">
+            <Label>Deploy to</Label>
+            <ChainSelector
+              onValueChange={setChainId}
+              disabled={pendingDeploy}
+            />
+          </div>
+          <DeployStep
+            pendingText="Resolve signer"
+            processingText="Resolving signer..."
+            completeText="Signer resolved"
+            state={signerState}
+          />
+          <DeployStep
+            pendingText="Wait for transaction"
+            processingText="Waiting for transaction..."
+            completeText="Transaction complete"
+            state={txnState}
+          />
+          <DeployStep
+            pendingText="Confirmation from the Tableland network"
+            processingText="Getting confirmation from the Tableland network..."
+            completeText="Tableland network confirmation received"
+            state={tblReceiptState}
+          />
+          <DeployStep
+            pendingText="Register Deployment with Studio"
+            processingText="Registering Deployment with Studio..."
+            completeText="Deployment registered with Studio"
+            state={recordDeploymentState}
+          />
           <DialogDescription>
-            Deploying a Table to Tableland will require you to sign and send a
-            transaction, as well as pay any transaction fees. Once the Table has
-            been deployed, it will be registered with your Studio Project and
-            you&apos;ll be able to view it in the Deployments tab.
+            <span className="font-semibold text-foreground">Important:</span>{" "}
+            Don&apos;t close or navigate away from this dialog while the
+            Deployment is executing. If you do, or if you cancel, you&apos;ll
+            likely still pay transaction fees and your table will still be
+            created on Tableland, but not be registered with Studio.
           </DialogDescription>
-        </DialogHeader>
-        <div className="flex items-center gap-2">
-          <Label>Deploy to</Label>
-          <ChainSelector onValueChange={setChainId} disabled={pendingDeploy} />
+          <DialogFooter>
+            <Button variant="outline" onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              // TODO: `handleSubmit` creates a floating promise, as a result the linter is complaining
+              //    we should figure out if this is ok or not and either change this or the lint config
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onClick={handleDeploy}
+              disabled={pendingDeploy || !chainId}
+            >
+              {pendingDeploy && (
+                <Loader2 className="mr-2 h-5 w-5 animate-spin" />
+              )}
+              Deploy
+            </Button>
+          </DialogFooter>
         </div>
-        <DeployStep
-          pendingText="Resolve signer"
-          processingText="Resolving signer..."
-          completeText="Signer resolved"
-          state={signerState}
-        />
-        <DeployStep
-          pendingText="Wait for transaction"
-          processingText="Waiting for transaction..."
-          completeText="Transaction complete"
-          state={txnState}
-        />
-        <DeployStep
-          pendingText="Confirmation from the Tableland network"
-          processingText="Getting confirmation from the Tableland network..."
-          completeText="Tableland network confirmation received"
-          state={tblReceiptState}
-        />
-        <DeployStep
-          pendingText="Register Deployment with Studio"
-          processingText="Registering Deployment with Studio..."
-          completeText="Deployment registered with Studio"
-          state={recordDeploymentState}
-        />
-        <DialogDescription>
-          <span className="font-semibold text-foreground">Important:</span>{" "}
-          Don&apos;t close or navigate away from this dialog while the
-          Deployment is executing. If you do, or if you cancel, you&apos;ll
-          likely still pay transaction fees and your table will still be created
-          on Tableland, but not be registered with Studio.
-        </DialogDescription>
-        <DialogFooter>
-          <Button variant="outline" onClick={handleCancel}>
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            // TODO: `handleSubmit` creates a floating promise, as a result the linter is complaining
-            //    we should figure out if this is ok or not and either change this or the lint config
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onClick={handleDeploy}
-            disabled={pendingDeploy || !chainId}
-          >
-            {pendingDeploy && <Loader2 className="mr-2 h-5 w-5 animate-spin" />}
-            Deploy
-          </Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
@@ -271,23 +278,25 @@ function DeployStep({
   return (
     <div
       className={cn(
-        "flex items-center gap-2",
+        "flex-auto items-center overflow-auto",
         state === "pending" && "opacity-40",
       )}
     >
-      {state === "pending" && (
-        <CircleDashed className="mr-2 h-5 w-5 flex-shrink-0" />
-      )}
-      {state === "processing" && (
-        <Loader2 className="mr-2 h-5 w-5 flex-shrink-0 animate-spin" />
-      )}
-      {state === "complete" && (
-        <CheckCircle2 className="mr-2 h-5 w-5 flex-shrink-0 text-green-500" />
-      )}
-      {state instanceof Error && (
-        <AlertCircle className="mr-2 h-5 w-5 flex-shrink-0 text-red-500" />
-      )}
-      <p className="text-sm">{text()}</p>
+      <div className="grid grid-cols-12">
+        {state === "pending" && (
+          <CircleDashed className="mr-2 h-5 w-5 flex-shrink-0" />
+        )}
+        {state === "processing" && (
+          <Loader2 className="mr-2 h-5 w-5 flex-shrink-0 animate-spin" />
+        )}
+        {state === "complete" && (
+          <CheckCircle2 className="mr-2 h-5 w-5 flex-shrink-0 text-green-500" />
+        )}
+        {state instanceof Error && (
+          <AlertCircle className="mr-2 h-5 w-5 flex-shrink-0 text-red-500" />
+        )}
+        <p className="col-span-11 text-sm">{text()}</p>
+      </div>
     </div>
   );
 
