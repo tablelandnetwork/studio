1984db13d74e32ccb4fb0186f5f615e7ad07f7a1
Joe Wagner <njwjs722@gmail.com>
2024-01-31 12:05:00 -0700

    (nonce) wip: adding redis commands

--- packages/nonce/src/main.ts
+++ packages/nonce/src/main.ts
@@ -1,80 +1,94 @@
-"use strict";
-
 import { ethers } from "ethers";
+import { kv } from "@vercel/kv";
 
 // @TODO: Keep a per-NonceManager pool of sent but unmined transactions for
 //        rebroadcasting, in case we overrun the transaction pool
 
 export class NonceManager extends ethers.Signer {
   readonly signer: ethers.Signer;
+  // NOTE: this kv instance is shared with all NonceManager instances
+  readonly memStore = kv;
 
-  _initialPromise: Promise<number>;
-  _deltaCount: number;
+  _initialPromise: Promise<number> | null;
 
   constructor(signer: ethers.Signer) {
     super();
-    this._deltaCount = 0;
+
+    // TODO: need to make sure the redis delta key for this signer address exists
+
     ethers.utils.defineReadOnly(this, "signer", signer);
-    ethers.utils.defineReadOnly(this, "provider", signer.provider || null);
+    ethers.utils.defineReadOnly(this, "provider", signer.provider ?? null);
   }
 
   connect(provider: ethers.providers.Provider): NonceManager {
-    return new NonceManager(this.signer.connect(provider));
+    this.signer.connect(provider);
   }
 
-  getAddress(): Promise<string> {
-    return this.signer.getAddress();
+  async getAddress(): Promise<string> {
+    return await this.signer.getAddress();
   }
 
-  getTransactionCount(blockTag?: ethers.providers.BlockTag): Promise<number> {
+  async getTransactionCount(
+    blockTag?: ethers.providers.BlockTag,
+  ): Promise<number> {
     if (blockTag === "pending") {
       if (!this._initialPromise) {
         this._initialPromise = this.signer.getTransactionCount("pending");
       }
-      const deltaCount = this._deltaCount;
-      return this._initialPromise.then((initial) => initial + deltaCount);
+      // TODO: this needs to be a findOrCreate where the created key is set to zero.
+      //       I don't know if redis can do this?
+      const deltaCount = await this.memStore.get(`delta:${await this.getAddress()}`);
+
+      // TODO: this `_initialPromise` concept might need to be replaced with
+      //       something that works across processes
+      const initial = await this._initialPromise;
+      return initial + (typeof deltaCount === "number" ? deltaCount : 0);
     }
 
-    return this.signer.getTransactionCount(blockTag);
+    return await this.signer.getTransactionCount(blockTag);
   }
 
-  setTransactionCount(
+  async setTransactionCount(
     transactionCount: ethers.BigNumberish | Promise<ethers.BigNumberish>,
-  ): void {
+  ): Promise<void> {
     this._initialPromise = Promise.resolve(transactionCount).then((nonce) => {
       return ethers.BigNumber.from(nonce).toNumber();
     });
-    this._deltaCount = 0;
+    // TODO: we can't set like this because different nonce manageer instances spread
+    //       across processes might be competing to set 0 and increment the delta
+    await this.memStore.set(`delta:${await this.getAddress()}`, 0);
   }
 
-  incrementTransactionCount(count?: number): void {
-    this._deltaCount += count == null ? 1 : count;
+  async incrementTransactionCount(count?: number): Promise<number> {
+    return await this.memStore.incrby(
+      `delta:${await this.getAddress()}`,
+      count == null ? 1 : count,
+    );
   }
 
-  signMessage(message: ethers.Bytes | string): Promise<string> {
-    return this.signer.signMessage(message);
+  async signMessage(message: ethers.Bytes | string): Promise<string> {
+    return await this.signer.signMessage(message);
   }
 
-  signTransaction(
+  async signTransaction(
     transaction: ethers.utils.Deferrable<ethers.providers.TransactionRequest>,
   ): Promise<string> {
-    return this.signer.signTransaction(transaction);
+    return await this.signer.signTransaction(transaction);
   }
 
-  sendTransaction(
+  async sendTransaction(
     transaction: ethers.utils.Deferrable<ethers.providers.TransactionRequest>,
   ): Promise<ethers.providers.TransactionResponse> {
     if (transaction.nonce == null) {
       transaction = ethers.utils.shallowCopy(transaction);
-      transaction.nonce = this.getTransactionCount("pending");
-      this.incrementTransactionCount();
+      transaction.nonce = await this.getTransactionCount("pending");
+      await this.incrementTransactionCount();
     } else {
-      this.setTransactionCount(transaction.nonce);
-      this._deltaCount++;
+      await this.setTransactionCount(transaction.nonce);
+      await this.memStore.incr(`delta:${await this.getAddress()}`);
     }
 
-    return this.signer.sendTransaction(transaction).then((tx) => {
-      return tx;
-    });
+    const tx = await this.signer.sendTransaction(transaction)
+    return tx;
   }
 }
