af15f53b80779dc9ab2c381194e02e88647f4723
Aaron Sutula <528969+asutula@users.noreply.github.com>
2024-02-29 11:21:35 -0600

    Table page v0.1 (#202)
    * table page v0.1
    
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>
    
    * Better name for table component
    
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>
    
    * lint fixes
    
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>
    Co-authored-by: Aaron Sutula <asutula@users.noreply.github.com>

--- packages/web/app/_components/latest-tables.tsx
+++ packages/web/app/_components/latest-tables.tsx
@@ -10,20 +10,10 @@ import ChainSelector from "@/components/chain-selector";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { type Table, getLatestTables } from "@/lib/validator-queries";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
 
 const timeAgo = new TimeAgo("en-US");
 
 export function LatestTables({ initialData }: { initialData: Table[] }) {
-  const [showAlert, setShowAlert] = useState(false);
   const [latestTables, setLatestTables] = useState<Table[]>(initialData);
   const [selectedChain, setSelectedChain] = useState<
     number | "mainnets" | "testnets"
@@ -49,20 +39,6 @@ export function LatestTables({ initialData }: { initialData: Table[] }) {
 
   return (
     <>
-      <AlertDialog open={showAlert} onOpenChange={setShowAlert}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Coming soon!</AlertDialogTitle>
-            <AlertDialogDescription>
-              We&apos;re working hard to lauch Tableland table pages as soon as
-              possible. Check back shortly.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogAction>Got it!</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
       <div className="mt-8 flex items-end gap-4">
         <TypographyH3>Latest Tableland tables</TypographyH3>
         <div className="ml-auto flex items-center gap-2">
@@ -81,12 +57,8 @@ export function LatestTables({ initialData }: { initialData: Table[] }) {
         {latestTables.slice(offset, offset + pageSize).map((table) => (
           <Link
             key={`${table.chain_id}-${table.id}`}
-            href={`/tables/${table.prefix}_${table.chain_id}_${table.id}`}
+            href={`/table/${table.prefix}_${table.chain_id}_${table.id}`}
             className="flex flex-col items-start gap-2 rounded-lg border p-4 text-left text-sm transition-all hover:bg-accent"
-            onClick={(e) => {
-              e.preventDefault();
-              setShowAlert(true);
-            }}
           >
             <div className="flex w-full flex-col gap-1">
               <div className="flex items-center gap-2">
---
+++ packages/web/app/table/[name]/page.tsx
@@ -0,0 +1,80 @@
+import { ApiError, type Table, Validator, helpers } from "@tableland/sdk";
+import TablelandTable from "@/components/tableland-table";
+
+export default async function TablePage({
+  params,
+}: {
+  params: { name: string };
+}) {
+  const parts = params.name.split("_");
+  if (parts.length < 3) {
+    return (
+      <div className="p-4">
+        <p>Invalid table name.</p>
+      </div>
+    );
+  }
+
+  // const [tokenId, chainIdString, ...rest] = [
+  const [tokenId, chainIdString] = [parts.pop()!, parts.pop()!, ...parts];
+  // const prefix = rest.join("_");
+  const chainId = parseInt(chainIdString, 10);
+  if (isNaN(chainId)) {
+    return (
+      <div className="p-4">
+        <p>Invalid chain id.</p>
+      </div>
+    );
+  }
+
+  const validator = new Validator({
+    baseUrl: helpers.getBaseUrl(chainId),
+  });
+
+  let tablelandTable: Table;
+  try {
+    tablelandTable = await validator.getTableById({
+      chainId,
+      tableId: tokenId,
+    });
+  } catch (err) {
+    if (err instanceof ApiError && err.status === 404) {
+      return (
+        <div className="p-4">
+          <p>
+            Table id {tokenId} not found on chain {chainId}.
+          </p>
+        </div>
+      );
+    }
+    console.error("Error getting table by id:", { err });
+    return (
+      <div className="p-4">
+        <p>Error getting table by id</p>
+      </div>
+    );
+  }
+
+  const createdAttr = tablelandTable.attributes?.find(
+    (attr) => attr.traitType === "created",
+  );
+  if (!createdAttr) {
+    return (
+      <div className="p-4">
+        <p>No created attribute found.</p>
+      </div>
+    );
+  }
+  const createdAt = new Date(createdAttr.value * 1000);
+
+  return (
+    <TablelandTable
+      displayName={params.name}
+      chainId={chainId}
+      createdAt={createdAt}
+      schema={tablelandTable.schema}
+      tableName={params.name}
+      tokenId={tokenId}
+    />
+  );
+}
