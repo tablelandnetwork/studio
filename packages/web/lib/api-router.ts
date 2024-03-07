2580925d49371b832501d5c3cfe924fcc190a241
Aaron Sutula <asutula@users.noreply.github.com>
2024-03-07 11:02:04 -0800

    better names for env vars
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- packages/web/lib/api-router.ts
+++ packages/web/lib/api-router.ts
@@ -11,6 +11,6 @@ export const apiRouter = appRouter(
   (seal) => `${baseUrl}/invite?seal=${seal}`,
   process.env.DATA_SEAL_PASS!,
   process.env.NODE_ENV === "development",
-  process.env.INFURA_KEY ?? "",
-  process.env.QUICK_NODE_KEY ?? "",
+  process.env.API_ROUTER_INFURA_KEY ?? "",
+  process.env.API_ROUTER_QUICK_NODE_KEY ?? "",
 );
