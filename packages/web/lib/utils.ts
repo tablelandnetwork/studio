5be593c1c350155502b3240406ba55853a556a26
Joe Wagner <njwjs722@gmail.com>
2024-03-08 16:51:06 -0700

    (web) regression fixes in data table

--- packages/web/lib/utils.ts
+++ packages/web/lib/utils.ts
@@ -6,15 +6,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-const objSchema = z.object({});
+const objSchema = z.record(z.string(), z.any());
+const arrSchema = z.array(z.any());
 // accepts an Array with any entries that are any kind of Object and goes
 // through each Object's keys to ensure that values containing an Object or
 // Array are `stringify`ed.  This enables showing nested data in an html table
 // without the dreded "[object Object]"
-export function objectToTableData<TData>(data: TData[]) {
-  if (!(data instanceof Array)) {
-    throw new Error("TypeError: data must be Array");
-  }
+export function objectToTableData<TData>(data: any[]) {
+  data = arrSchema.parse(data);
 
   return data.map(function (d) {
     return Object.fromEntries(
