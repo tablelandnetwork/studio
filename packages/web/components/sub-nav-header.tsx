6b2aac59d0e2fe00726d573d0318e95773e8b763
Joe Wagner <njwjs722@gmail.com>
2024-03-30 09:20:02 -0600

    (web) fix transparent sub-nav header

--- packages/web/components/sub-nav-header.tsx
+++ packages/web/components/sub-nav-header.tsx
@@ -4,7 +4,7 @@ export default function SubNavHeader({
   children: React.ReactNode;
 }) {
   return (
-    <header className="sticky top-0 flex flex-col space-y-4 border-b border-[#080A1E] bg-[#202132] px-4 py-3">
+    <header className="z-50 sticky top-0 flex flex-col space-y-4 border-b border-[#080A1E] bg-[#202132] px-4 py-3">
       <div className="flex">{children}</div>
     </header>
   );
