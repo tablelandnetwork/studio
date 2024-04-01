6d2f5e272bd3f6a66b10ab3bb27dcb78cd4a756e
Aaron Sutula <asutula@users.noreply.github.com>
2024-04-01 17:12:52 -0600

    new team form in sheet
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- packages/web/components/time.tsx
+++ packages/web/components/time.tsx
@@ -5,17 +5,17 @@ import TimeAgo from "javascript-time-ago";
 
 export function TimeSince({
   time,
-}: React.HTMLAttributes<HTMLElement> & {
+  ...rest
+}: React.HTMLAttributes<HTMLSpanElement> & {
   time: string | number | Date;
 }) {
   const [timeStr, setTimeStr] = useState("");
   const timeAgo = new TimeAgo("en-US");
 
   useEffect(function () {
-    if (!(time instanceof Date)) time = new Date(time);
-
-    setTimeStr(timeAgo.format(time));
+    const date = time instanceof Date ? time : new Date(time);
+    setTimeStr(timeAgo.format(date));
   });
 
-  return <span>{timeStr}</span>;
+  return <span {...rest}>{timeStr}</span>;
 }
