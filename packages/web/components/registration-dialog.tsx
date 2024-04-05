9dfc8cd67685b6390df6fbad74708a7f41ce4577
Aaron Sutula <asutula@users.noreply.github.com>
2024-04-05 12:02:39 -0600

    register form default email to undefined
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- packages/web/components/registration-dialog.tsx
+++ packages/web/components/registration-dialog.tsx
@@ -45,7 +45,7 @@ export default function RegistrationDialog({
     resolver: zodResolver(registerSchema),
     defaultValues: {
       username: "",
-      email: "",
+      email: undefined,
     },
   });
 
